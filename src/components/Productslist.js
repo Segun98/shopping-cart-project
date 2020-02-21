e469551e98ba3e354158141789d1f549036af8ee
Segun98 <53624548+Segun98@users.noreply.github.com>
2020-02-21 22:19:44 +0100

    prevent duplicates in cart

--- src/components/Productslist.js
+++ src/components/Productslist.js
@@ -5,23 +5,33 @@ function Productslist({ src, name, price, quantity, subtotal, id, index}) {
   const [Cart, setCart] = useContext(CartContext);
 
   // ADD PRODUCT TO CART
-  function addToCart() {
-    let newCart = {
-      quantity,
-      src,
-      name,
-      price,
-      subtotal,
-      id
-    };
-    if (Cart.includes(Cart[index])) {
+  function addToCart(product) {
+    // let newCart = {
+    //   quantity,
+    //   src,
+    //   name,
+    //   price,
+    //   subtotal,
+    //   id
+    // };
+    const existing = Cart.filter(c=> c.id === product.id)
+    if (existing.length > 0) {
+      const without = Cart.filter(c=> c.id !== product.id);
+      setCart([...without, ...existing])
       alert(`${name.toUpperCase()} IS ALREADY IN YOUR CART!!`);
     } else {
-      setCart([...Cart, newCart]);
+      setCart([...Cart, product])
       alert(`${name.toUpperCase()} HAS BEEN ADDED TO CART!!!!`);
     }
-
-    //THIS IS ME TRYING TO PREVENT DUPLICATES IN THE ARRAY . IM TIREDDDD
+    
+    //THIS WAS ME TRYING TO PREVENT DUPLICATES IN THE ARRAY . IM WAS TIREDDDD BUT NEVER GAVE UP
+    
+    // if (Cart.includes(Cart[index])) {
+    //   alert(`${name.toUpperCase()} IS ALREADY IN YOUR CART!!`);
+    // } else {
+    //   setCart([...Cart, newCart]);
+    //   alert(`${name.toUpperCase()} HAS BEEN ADDED TO CART!!!!`);
+    // }
 
     //newCart.filter((item, index)=> newCart.indexOf(item) === index)
     // const without = Cart.filter((item, index)=> Cart.indexOf(item) !== index)
@@ -36,14 +46,13 @@ function Productslist({ src, name, price, quantity, subtotal, id, index}) {
     //newCart.filter((it, index)=> newCart.indexOf(it) === index)
     //Cart.reduce((Cart, index)=> Cart.includes(index)? Cart : setCart([...Cart,newCart]),[])
   }
-
   return (
     <div>
       <div className="product-item">
         <h4>{name}</h4>
         <img src={src} alt={name} />
         <h3>N{price}</h3>
-        <button onClick={addToCart}>ADD TO CART</button>
+        <button onClick={()=>addToCart({src, name, price, quantity, subtotal, id})}>ADD TO CART</button>
       </div>
     </div>
   );
